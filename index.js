154a0b30092880b08edf7725a688b600b23f05b9
James Titus <titusjames299@gmail.com>
2024-08-11 15:00:41 +0000

    updated conn string

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-require('dotenv').config()
+require("dotenv").config();
+const mysql = require("mysql2");
+const express = require("express");
 
-const mysql = require('mysql2');
-const express = require('express');
 const app = express();
-app.use(express.json())
-
-// Use port 8080 by default, unless configured differently in Google Cloud
+app.use(express.json());
 const port = process.env.PORT || 8080;
+
+const connectionString = process.env.CONN_STRING;
+const connection = mysql.createConnection(connectionString);
+
+app.get("/status", async (req, res) => {
+  const query = "show tables";
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    // res.json((data = results[0]));
+    if(results[0]){
+      res.json("Success");
+    }
+  });
+});
+
+app.get("/", async (req, res) => {
+  const query = "select * from banneritems where expiration_time > now()";
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.json((data = results[0]));
+  });
+});
+
 app.listen(port, () => {
-   console.log(`App is running at: http://localhost:${port}`);
+  console.log(`App is running at: http://localhost:${port}`);
 });
 
-const pool = mysql.createPool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  databse: process.env.DB_NAME,
-  socketPath: `/cloudsql/${process.env.INSTANCE_CONN_NAME}`,
-});
-
-// console.log(pool);
-
-app.get('/status', async (req, res) => res.send('Success.') );
-
-app.get('/', async (req, res) => {
-  try {
-    const [rows, fields] = await pool.execute(query);
-    res.json({ data: rows }); // Send the results as JSON
-  } catch (error) {
-    console.error('Error executing query:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
+// connection.end();
